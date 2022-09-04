e24a9a02059497a04cbc9324d518e75e9f07f04b
Bryce Maxwell <brycemaxwell@Bryces-MBP.hitronhub.home>
2022-09-04 17:55:27 -0500

    first commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,32 @@
+# for i in range(1, 51):
+#     print(i)
+
+# for i in range (5, 1001):
+#     if i % 5 == 0:
+#         print(i)
+
+# for i in range (1,101):
+#     if i % 5 == 0:
+#         print ('Coding')
+#     if i % 10 == 0:
+#         print ('Dojo')
+#     else:
+#         print (i)
+
+# s=0
+# for i in range (0,5001):
+#     if i%2==1:
+#         s+=i
+# print(s)
+
+
+# for i in range (2018,0, -4):
+#     print (i)
+
+lowNum=2
+highNum=9
+mult=3
+
+for i in range (lowNum,highNum+1):
+    if i%mult==0:
+        print(i)
